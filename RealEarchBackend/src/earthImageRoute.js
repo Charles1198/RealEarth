cedc0ac7a00da29a8765be0453b003acf09ce019
Charles1198 <charles@bqteam.com>
2024-12-07 22:37:58 +0800

    add

--- RealEarchBackend/src/earthImageRoute.js
+++ RealEarchBackend/src/earthImageRoute.js
@@ -6,19 +6,26 @@ const client = ossClient()
 const router = new Router({ prefix: '/earth' })
 
 router.get('/past24hour', async (ctx) => {
-  const result = await client.listV2({ 'max-keys': 200, prefix: 'past24hour/' })
+  try {
+    const result = await client.listV2({ 'max-keys': 200, prefix: 'past24hour/' })
 
-  if (!result) {
-    ctx.body = failedResp(500, 'no result')
-    return
-  }
+    if (!result) {
+      ctx.body = failedResp(500, 'no result')
+      return
+    }
 
-  const { res, objects } = result
+    const { res, objects } = result
 
-  if (res.statusCode === 200) {
-    ctx.body = successResp(objects.reverse())
-  } else {
-    ctx.body = failedResp(res.statusCode, res.statusMessage)
+    if (res.statusCode === 200) {
+      const imageList = objects.map((item) => {
+        return { size: item.size, url: item.url }
+      }).reverse()
+      ctx.body = successResp(imageList)
+    } else {
+      ctx.body = failedResp(res.statusCode, res.statusMessage)
+    }
+  } catch (error) {
+    ctx.body = failedResp(500, error.message)
   }
 })
 
@@ -35,7 +42,3 @@ function failedResp(code, message) {
 }
 
 module.exports = router
-
-// module.exports = function (app) {
-//   app.use(router).use(router.allowedMethods())
-// }
--- RealEarchBackend/src/ossClient.js
+++ RealEarchBackend/src/ossClient.js
@@ -2,7 +2,7 @@ const OSS = require('ali-oss')
 
 module.exports = function () {
   return new OSS({
-    region: 'oss-cn-beijing-internal.aliyuncs.com',
+    region: 'oss-cn-beijing',
     accessKeyId: process.env.ALIBABA_CLOUD_ACCESS_REAL_EARTH_KEY_ID,
     accessKeySecret: process.env.ALIBABA_CLOUD_ACCESS_REAL_EARTH_KEY_SECRET,
     authorizationV4: true,
